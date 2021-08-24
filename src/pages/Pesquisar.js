ed2f2051f97cbfee75b7b43640dbcbb14e3d30e9
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-08-23 22:11:48 -0300

    Create Pesquisar.js

---
+++ src/pages/Pesquisar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pesquisar() {
+  return (
+      <div class="pesquisar">
+        <input type="text" id="pesquisar" class="pesquisa" placeholder="Pesquisar..."/>     
+      </div>   
+  );
+}
+
+export default Pesquisar;
