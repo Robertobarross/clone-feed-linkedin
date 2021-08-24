e9bcd211615f12faf30b13a675fe7b4ef7100e7b
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-08-23 22:10:13 -0300

    Create Body.js

---
+++ src/pages/Body.js
@@ -0,0 +1,14 @@
+// Criei o Body para estilizar o plaono de fundo da página
+import React from 'react';
+
+function Body() {
+    return (
+      <div>
+          <div class="body">
+          </div>
+      </div>
+    );
+  }
+  
+  export default Body;
+  
